cf4db8363016be0e8332a9560dbede49c1aaaa13
Tae Young, Kim (casper) <kty0529@gmail.com>
2023-11-15 14:19:22 +0900

    refactor: 기본 구성 변경

---
+++ src/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// assets
+import "./assets/scss/reset.scss";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+      <div className="container">
+        content
+      </div>
+  </React.StrictMode>
+);
