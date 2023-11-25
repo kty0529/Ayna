ab7150b369d29bd178b1ecee87dbb9a276be758b
Tae Young, Kim (casper) <kty0529@gmail.com>
2023-11-25 17:18:19 +0900

    feat: 노션 데이터베이스 불러오기

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 import * as config from "../../config";
 import style from "./Home.module.scss";
+import {useQuery} from "react-query";
 
 // Components
 import Header from "../../components/Header/Header";
@@ -7,6 +8,19 @@ import Footer from "../../components/Footer/Footer";
 import ListItem from "../../components/ListItem/ListItem";
 
 function Home() {
+  const {isLoading, data} = useQuery("buttonData", async () => {
+    return await fetch(process.env.REACT_APP_NOTION_API)
+      .then(res => res.json())
+  });
+
+
+  let reverseData = [];
+  if ( ! isLoading ) {
+    console.log(data.results);
+    reverseData = [...data.results].reverse();
+  }
+
+
   return (
     <>
       <Header />
@@ -17,25 +31,45 @@ function Home() {
           <br />자세한 내용은 인스타그램을 확인해주세요. 😊
         </div>
 
-        <ul className={style.lists}>
-          {
-            config.ButtonData.map((item, index) => {
-              if (item.separator) {
-                return (
-                  <li key={index} className={style.separator}></li>
-                )
+        {
+          isLoading ? (
+            <div>로딩중...</div>
+          ) : (
+            <ul className={style.lists}>
+              {
+                reverseData.map((item, index) => {
+                  console.log(index, "Display: ", item.properties?.Display?.checbox);
+                  console.log(index, "ID: ", item.properties?.ID?.unique_id?.number, item?.properties?.ID?.unique_id?.prefix);
+                  console.log(index, "Name: ", item.properties?.Name?.title[0]?.plain_text);
+                  console.log(index, "NameBoder: ", item.properties?.NameBolder?.checkbox);
+                  console.log(index, "SubTitle: ", item.properties?.SubTitle?.rich_text[0]?.plain_text);
+                  console.log(index, "Thumbnail: ", item.properties?.Thumbnail?.files[0]?.name, item.properties?.Thumbnail?.files[0]?.type, item.properties?.Thumbnail?.files[0]?.file?.url);
+                  console.log(index, "Type: ", item.properties?.Type?.select?.name);
+                  console.log(index, "URL: ", item.properties?.URL?.url);
+                  console.log(index, "URLTarget: ", item.properties?.URLTarget?.select?.name);
+                })
               }
 
-              return (
-                <li key={index}>
-                  <ListItem
-                    {...item}
-                  />
-                </li>
-              )
-            })
-          }
-        </ul>
+              {
+                config.ButtonData.map((item, index) => {
+                  if (item.separator) {
+                    return (
+                      <li key={index} className={style.separator}></li>
+                    )
+                  }
+
+                  return (
+                    <li key={index}>
+                      <ListItem
+                        {...item}
+                      />
+                    </li>
+                  )
+                })
+              }
+            </ul>
+          )
+        }
       </main>
 
       <Footer />
